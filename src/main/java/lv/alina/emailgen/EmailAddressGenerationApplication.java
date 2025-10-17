96bda39bfe3a406d0531c8dca13195bb0b7074b8
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-10-17 13:32:25 +0300

    first commit

---
+++ src/main/java/lv/alina/emailgen/EmailAddressGenerationApplication.java
@@ -0,0 +1,13 @@
+package lv.alina.emailgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAddressGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAddressGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/alina/emailgen/EmailAddressGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.alina.emailgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAddressGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
