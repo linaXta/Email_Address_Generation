1b5290f25db7e099848ffe55a4f8f426ff0ad00d
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:43:47 +0200

    IShortCodesRepo added

--- src/main/java/lv/alina/emailgen/repos/IShortCodesRepo.java
+++ src/main/java/lv/alina/emailgen/repos/IShortCodesRepo.java
@@ -3,7 +3,12 @@ package lv.alina.emailgen.repos;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.alina.emailgen.models.ShortCodes;
+import lv.alina.emailgen.models.User;
 
 public interface IShortCodesRepo extends CrudRepository<ShortCodes, Long> {
+	
+	boolean existsByUserAndCode(User user, String shortCode);
+	
+	ShortCodes findByUserAndShortCode(User user, String shortCode);
 
 }
