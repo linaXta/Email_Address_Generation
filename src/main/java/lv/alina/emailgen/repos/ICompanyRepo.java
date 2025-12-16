a1e6fd6018190df5453d9c48c1635c10602f2963
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:06:04 +0200

    Company repo

--- src/main/java/lv/alina/emailgen/repos/ICompanyRepo.java
+++ src/main/java/lv/alina/emailgen/repos/ICompanyRepo.java
@@ -5,5 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import lv.alina.emailgen.models.Company;
 
 public interface ICompanyRepo extends CrudRepository<Company, Long> {
+	
+	Company findByNameIgnoreCase(String name);
+	
+	Company findByShortCode(String shortCode);
+	
+	boolean existByShortCode(String shortCode);
+	
+	boolean existByNameIgnoreCase(String name);
 
 }
