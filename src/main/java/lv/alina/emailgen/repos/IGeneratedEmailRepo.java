5e1c39870fd0c0126849c726627398d3cb051ecb
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:22:24 +0200

    IGeneratedEmailRepo added

--- src/main/java/lv/alina/emailgen/repos/IGeneratedEmailRepo.java
+++ src/main/java/lv/alina/emailgen/repos/IGeneratedEmailRepo.java
@@ -2,8 +2,13 @@ package lv.alina.emailgen.repos;
 
 import org.springframework.data.repository.CrudRepository;
 
+import lv.alina.emailgen.models.Company;
 import lv.alina.emailgen.models.GeneratedEmail;
 
 public interface IGeneratedEmailRepo extends CrudRepository<GeneratedEmail, Long>{
+	
+	boolean existsByEmail(String email);
+	
+	boolean existsByCompanyAndEmail(String email, Company company);
 
 }
