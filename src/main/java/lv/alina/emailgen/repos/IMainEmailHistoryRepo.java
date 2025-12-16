81b76e4b20e5ff20161ffadab0a5b3b146e67a3c
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:26:53 +0200

    IMainEmailHistoryRepo updated

--- src/main/java/lv/alina/emailgen/repos/IMainEmailHistoryRepo.java
+++ src/main/java/lv/alina/emailgen/repos/IMainEmailHistoryRepo.java
@@ -11,27 +11,23 @@ import lv.alina.emailgen.models.MainEmailHistory.ActionType;
 
 
 public interface IMainEmailHistoryRepo extends CrudRepository<MainEmailHistory, Long> {
-	// Visi ieraksti: jaunākas -> vecākais
+	
     List<MainEmailHistory> findAllByMainEmailOrderByUpdatedAtDesc(MainEmail mainEmail);
 
-    // Visi ieraksti: vecākais -> jaunākais
     List<MainEmailHistory> findAllByMainEmailOrderByUpdatedAtAsc(MainEmail mainEmail);
 
-    // Filtrs pēc darbības tipa (UPDATE/DELETE), jaunākais -> vecākais
     List<MainEmailHistory> findAllByMainEmailAndActionTypeOrderByUpdatedAtDesc(
             MainEmail mainEmail, ActionType actionType
     );
 
-    // Perioda filtrs, jaunākais -> vecākais
     List<MainEmailHistory> findAllByMainEmailAndUpdatedAtBetweenOrderByUpdatedAtDesc(
             MainEmail mainEmail, LocalDateTime from, LocalDateTime to
     );
 
-    // Perioda filtrs, vecākais -> jaunākais
     List<MainEmailHistory> findAllByMainEmailAndUpdatedAtBetweenOrderByUpdatedAtAsc(
             MainEmail mainEmail, LocalDateTime from, LocalDateTime to
     );
 
-    long countByMainEmail(MainEmail mainEmail);// skaits
+    long countByMainEmail(MainEmail mainEmail);
 
 }
