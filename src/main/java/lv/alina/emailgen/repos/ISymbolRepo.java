abc133ff6d02fc8f779e16bb04671459cd385e0e
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:51:39 +0200

    ISymbolRepo added

--- src/main/java/lv/alina/emailgen/repos/ISymbolRepo.java
+++ src/main/java/lv/alina/emailgen/repos/ISymbolRepo.java
@@ -3,7 +3,12 @@ package lv.alina.emailgen.repos;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.alina.emailgen.models.Symbol;
+import lv.alina.emailgen.models.User;
 
 public interface ISymbolRepo extends CrudRepository<Symbol, Long> {
+	
+	boolean existsByUserAndSymbol(User user, String symbol);
+	
+	Symbol findByUserAndSymbol(User user, String symbol);
 
 }
