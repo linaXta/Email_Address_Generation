02d98358b5b4ff15526e39a57b87e558da3b894a
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-16 10:37:02 +0200

    IMainEmailRepo added

--- src/main/java/lv/alina/emailgen/repos/IMainEmailRepo.java
+++ src/main/java/lv/alina/emailgen/repos/IMainEmailRepo.java
@@ -1,12 +1,16 @@
 package lv.alina.emailgen.repos;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
+
+import lv.alina.emailgen.models.Company;
 import lv.alina.emailgen.models.MainEmail;
 
 public interface IMainEmailRepo extends CrudRepository<MainEmail, Long>{
-    List<MainEmail> findByUserUserId(Long userId);
-    MainEmail findByMainEmail(String mainEmail);
+	
+	MainEmail findByEmail(String email);
+    
+	boolean existsByEmail(String email);
+	
+	boolean existsByCompanyAndEmail(String email, Company company);
 
 }
