79baebe10c279de96d6666ca3dda3c03f6f0737f
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-30 14:55:01 +0200

    vaadin page is opening

--- src/main/java/lv/alina/emailgen/views/admin/UserAdminView.java
+++ src/main/java/lv/alina/emailgen/views/admin/UserAdminView.java
@@ -1,6 +1,7 @@
 package lv.alina.emailgen.views.admin;
 
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -8,6 +9,22 @@ import com.vaadin.flow.router.Route;
 @Route("admin/users")
 @CssImport("./styles/admin.css")
 public class UserAdminView extends VerticalLayout {
+	
+	public UserAdminView() {
+        addClassName("admin-page");
+        setSizeFull();
+
+        VerticalLayout card = new VerticalLayout();
+        card.addClassName("admin-card");
+        card.setWidthFull();
+        card.setMaxWidth("1100px");
+
+        H1 title = new H1("User admin panel");
+        title.addClassName("admin-title");
+
+        card.add(title);
+        add(card);
+    }
 
     
 }
