29010fe7a056c43c46dfe03146f75ba260c9b648
Alīna Rubcova <alina.rubcova@testdevlab.com>
2025-12-30 14:46:16 +0200

    Ignore Vaadin generated files and add admin styles

--- src/main/java/lv/alina/emailgen/views/admin/UserAdminView.java
+++ src/main/java/lv/alina/emailgen/views/admin/UserAdminView.java
@@ -1,14 +1,13 @@
 package lv.alina.emailgen.views.admin;
 
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("admin/users")
-public class UserAdminView extends VerticalLayout{
-	
-	public UserAdminView() {
-        add(new H1("User admin panel"));
-    }
 
+@Route("admin/users")
+@CssImport("./styles/admin.css")
+public class UserAdminView extends VerticalLayout {
+
+    
 }
